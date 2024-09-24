b177eec5087da5e969ec698529746a7e3c77903d
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-09-24 17:00:15 +0200

    [UPD] A header "component" is now on the page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel-comics</title>
+
+    @vite('resources/js/app.js')
 </head>
 
 <body>
-    
+    @include('partials.header')
+    <main>
+        
+    </main>
 </body>
 
 </html>
